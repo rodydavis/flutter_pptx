601e82c991e8a918b49fdb259c9184e4dc45e16d
Rody Davis <rody.davis.jr@gmail.com>
2023-04-02 01:58:33 -0400

    adding method for widget

--- packages/flutter_pptx/lib/src/powerpoint.dart
+++ packages/flutter_pptx/lib/src/powerpoint.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:dart_pptx/dart_pptx.dart' show PowerPoint;
 import 'package:flutter_pptx/flutter_pptx.dart';
 // ignore: implementation_imports
@@ -16,24 +18,43 @@ class FlutterPowerPoint extends PowerPoint {
     BuildContext? context,
     Size? targetSize,
   }) async {
-    final ctx = this.context as FlutterPresentationContext;
-    final size = targetSize ??
-        Size(
-          Util.ptToPixle(layout.width),
-          Util.ptToPixle(layout.height),
-        );
-    final bytes = await ctx.screenshotController.captureFromWidget(
-      builder(size),
+    final image = await imageFromWidget(
+      this,
+      builder,
       delay: delay,
       pixelRatio: pixelRatio,
       context: context,
-      targetSize: size,
-    );
-    final image = ImageReference.fromBytes(
-      bytes,
-      name: 'widget',
-      description: 'image created from a widget',
+      targetSize: targetSize,
     );
     return addSlide(SlideBlank()..background.image = image);
   }
+}
+
+Future<ImageReference> imageFromWidget(
+  FlutterPowerPoint pres,
+  Widget Function(Size size) builder, {
+  Duration delay = const Duration(seconds: 1),
+  double? pixelRatio,
+  BuildContext? context,
+  Size? targetSize,
+}) async {
+  final ctx = pres.context as FlutterPresentationContext;
+  final size = targetSize ??
+      Size(
+        Util.ptToPixle(pres.layout.width),
+        Util.ptToPixle(pres.layout.height),
+      );
+  final bytes = await ctx.screenshotController.captureFromWidget(
+    builder(size),
+    delay: delay,
+    pixelRatio: pixelRatio,
+    context: context,
+    targetSize: size,
+  );
+  final image = ImageReference.fromBytes(
+    bytes,
+    name: 'widget',
+    description: 'image created from a widget',
+  );
+  return image;
 }
